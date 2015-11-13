e940264a0db0ad4ce0445b46a633e00eeca95ae6
Randhir Singh <randhir@betasoftsystems.com>
2015-11-13 05:57:37 +0530

    Html for home page and forgot password functionality

--- app/assets/javascripts/angular/app.js
+++
@@ -1,7 +0,0 @@
-
-/*
- * Start angular application
- */
-
-//var espApp = angular.module('eps_app', ['ngRoute']);
-
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
-	before_filter :current_user
-	before_filter :authenticate_user!
+	#before_filter :current_user
+	#before_filter :authenticate_user!
 
 	def index
 		render :template => "welcome/index", :locals => { :user => @current_user }
