998c1687d192f254b6ad20946c445bf42921982b
betasoft111 <betasoft111@gmail.com>
2015-12-03 02:08:25 +0530

    search page done

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,11 +5,7 @@ class SearchController < ApplicationController
 	#  	        Search method       #
 	#################################
 	def search     
-		@search_results = CompanyStaff.where("skills LIKE ? OR availability like ? OR experience like ? OR qualification like ?", "%#{params[:skills]}%", "%#{params[:availability]}%" , "%#{params[:experience]}%" , "%#{params[:staff_quf]}%" )
+		@search_results = CompanyStaff.find_by_sql "SELECT users.first_name as company_fn, users.last_name as company_ln, company_staffs.* FROM users 
+			INNER JOIN company_staffs ON users.id = company_staffs.company_id where company_staffs.skills like '%#{params[:skills]}%' OR company_staffs.availability like '%#{params[:availability]}%' OR company_staffs.experience like '%#{params[:experience]}%' OR company_staffs.qualification like '%#{params[:staff_quf]}%'"
 	end
-
-
-
-
-
 end
