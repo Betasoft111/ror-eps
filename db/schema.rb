10cfdf6052b0d06f94d6f90295b1a2d9037eb26c
Randhir Singh <randhir@betasoftsystems.com>
2015-12-05 03:45:45 +0530

    desinging updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203193108) do
+ActiveRecord::Schema.define(version: 20151204151147) do
 
   create_table "admin_admin_users", force: true do |t|
     t.string   "first_name"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20151203193108) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "plan_type"
+    t.integer  "total_profiles"
   end
 
   create_table "user_payments", force: true do |t|
