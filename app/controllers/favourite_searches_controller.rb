7291971cce97895e922cf958fe9827ea05258a0b
betasoft111 <betasoft111@gmail.com>
2015-12-16 01:00:51 +0530

    worked on show fevrute search on front end working on searches

--- app/controllers/favourite_searches_controller.rb
+++ app/controllers/favourite_searches_controller.rb
@@ -1,8 +1,17 @@
 class FavouriteSearchesController < ApplicationController
+	before_filter :authenticate_user!
+	before_filter :current_user
+	
 
 	#################################
 	#       Save Search Result	    #
 	#################################
+
+	def index
+	@logedinusers_FavouriteSearch =	FavouriteSearch.where(:user_id => @current_user.id )
+
+	end
+
 	
 	def create
 		favourite_search = FavouriteSearch.new(favourite_params) if current_user
@@ -12,6 +21,11 @@ class FavouriteSearchesController < ApplicationController
 			redirect_to request.referrer, :notice => "Sorry, please try again"
 		end
 	end
+
+
+
+
+
 
 	private
  	
