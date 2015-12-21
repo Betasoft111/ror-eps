22afdb031667ed79b07b237def17c24bb5a162c4
Randhir Singh <randhir@betasoftsystems.com>
2015-12-21 23:53:18 +0530

    New updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
     if @current_user != nil
       @current_user
     else
-      redirect_to '/join', :notice => 'Please login to view this'
+      redirect_to '/sign_in', :notice => 'Please login to view this'
       ## if you want render 404 page
       ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
     end
